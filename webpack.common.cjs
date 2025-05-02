2087ad63244914853143fe288ec836b2b3a73307
syeero7 <EmalHasitha@proton.me>
2025-05-02 06:17:42 +0530

    build: configure to include favicon

--- webpack.common.cjs
+++ webpack.common.cjs
@@ -13,6 +13,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/main.html",
+      favicon: "./src/assets/favicon.ico",
     }),
     new MiniCssExtractPlugin(),
   ],
